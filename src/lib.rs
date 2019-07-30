4059acdcbcdea32c49f5191ecd33cc98d60086a4
David Kellum <dek-oss@gravitext.com>
2019-07-30 15:12:42 -0700

    pc32 equivelent (no TSAN error)

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 #[cfg(test)]
 mod tests {
 
-    use rand::thread_rng;
-    use rand::Rng;
+    use rand_pcg::Lcg64Xsh32 as Pcg32;
+    use rand_core::{RngCore, SeedableRng};
 
     use std::thread;
     use std::time::Duration;
 
     #[test]
     fn hammer() {
-        let threads: Vec<_> = (0..100).map(|_| thread::spawn(|| {
-            let ms = thread_rng().gen_range(3, 12);
+        let threads: Vec<_> = (0..100).map(|i| thread::spawn(move || {
+            let mut rng = Pcg32::seed_from_u64(i);
+            let ms = rng.next_u64() % 10 as u64 + 3;
             thread::sleep(Duration::from_millis(ms));
         })).collect();
 
