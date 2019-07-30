22e2ed46cc7f7766802f9bf166a2ec3a49c7fcab
David Kellum <dek-oss@gravitext.com>
2019-07-30 15:12:42 -0700

    RandomState and LazyStatic

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # rng-tsan-min-repro
 
-Minimized reproduction of a thread sanitizer issue on getrandom 0.1.7:
+Minimized reproduction of a thread sanitizer issue involving c2-chacha
+(via rand_chacha).
 
 Steps to reproduce with (rust/LLVM) thread sanitizer:
 
@@ -9,13 +10,8 @@ export TSAN_OPTIONS="suppressions=`pwd`/tsan"
 RUST_BACKTRACE=1 RUSTFLAGS="-Z sanitizer=thread" cargo test
 ```
 
-To make the error go away, downgrade getrandom crate:
-
-``` bash
-cargo update -p getrandom --precise 0.1.6
-```
-
-Or this can be added to the suppressions file, but so far seems under-specific:
+If this is a false positive, this can be added to the suppressions file, but so
+far seems under-specific:
 
 ``` txt
 # See https://github.com/dekellum/rng-tsan-min-repro
--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,29 @@
 #[cfg(test)]
 mod tests {
 
-    use rand::thread_rng;
-    use rand::Rng;
+    use lazy_static::lazy_static;
 
+    use rand_pcg::Lcg64Xsh32 as Pcg32;
+    use rand_core::{RngCore, SeedableRng};
+
+    use std::collections::hash_map::RandomState;
+    use std::hash::{BuildHasher, Hash, Hasher};
     use std::thread;
     use std::time::Duration;
 
+    lazy_static! {
+        static ref RND_STATE: RandomState = RandomState::new();
+    }
+
     #[test]
     fn hammer() {
-        let threads: Vec<_> = (0..100).map(|_| thread::spawn(|| {
-            let ms = thread_rng().gen_range(3, 12);
+
+        let threads: Vec<_> = (0..100).map(|i| thread::spawn(move || {
+            let mut hasher = RND_STATE.build_hasher();
+            i.hash(&mut hasher);
+            let hash: u64 = hasher.finish();
+            let mut rng = Pcg32::seed_from_u64(hash);
+            let ms = rng.next_u64() % 10 as u64 + 3;
             thread::sleep(Duration::from_millis(ms));
         })).collect();
 
