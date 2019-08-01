8d35316421035b4461c030b6a0614ee612aeacc6
David Kellum <dek-oss@gravitext.com>
2019-08-01 10:10:19 -0700

    just RandomState

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,6 @@
 #[cfg(test)]
 mod tests {
 
-    use lazy_static::lazy_static;
-
     use rand_pcg::Lcg64Xsh32 as Pcg32;
     use rand_core::{RngCore, SeedableRng};
 
@@ -11,15 +9,11 @@ mod tests {
     use std::thread;
     use std::time::Duration;
 
-    lazy_static! {
-        static ref RND_STATE: RandomState = RandomState::new();
-    }
-
     #[test]
     fn hammer() {
-
         let threads: Vec<_> = (0..100).map(|i| thread::spawn(move || {
-            let mut hasher = RND_STATE.build_hasher();
+            let rnd_state = RandomState::new();
+            let mut hasher = rnd_state.build_hasher();
             i.hash(&mut hasher);
             let hash: u64 = hasher.finish();
             let mut rng = Pcg32::seed_from_u64(hash);
