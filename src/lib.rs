94c4e5c09b3714cd288df04ce4b963bb20be1792
David Kellum <dek-oss@gravitext.com>
2019-08-01 10:22:38 -0700

    just LazyStatic

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,19 @@
 mod tests {
 
     use lazy_static::lazy_static;
-
-    use rand_pcg::Lcg64Xsh32 as Pcg32;
-    use rand_core::{RngCore, SeedableRng};
-
-    use std::collections::hash_map::RandomState;
-    use std::hash::{BuildHasher, Hash, Hasher};
     use std::thread;
+
     use std::time::Duration;
 
     lazy_static! {
-        static ref RND_STATE: RandomState = RandomState::new();
+        static ref LAZY_STATE: u64 = 2;
     }
 
     #[test]
     fn hammer() {
-
         let threads: Vec<_> = (0..100).map(|i| thread::spawn(move || {
-            let mut hasher = RND_STATE.build_hasher();
-            i.hash(&mut hasher);
-            let hash: u64 = hasher.finish();
-            let mut rng = Pcg32::seed_from_u64(hash);
-            let ms = rng.next_u64() % 10 as u64 + 3;
-            thread::sleep(Duration::from_millis(ms));
+            let j: u64 = *LAZY_STATE;
+            thread::sleep(Duration::from_micros(i + j));
         })).collect();
 
         for t in threads {
